9868cb414045224ceeeb080092a05bf063526884
GhoulPoP <dylan@lamarca.org>
2016-07-28 13:52:57 -0700

    Serial Communication with Frequency
    Improved the Serial communication between the Python and the Arduino so
    that the hertz and duty cycle of the strobe is dictated by the serial
    input

--- ArduinoController.py
+++ ArduinoController.py
@@ -2,5 +2,14 @@ import serial
 import time
 
 ser = serial.Serial('COM6',9600)
+time.sleep(5)
+print('Write: 30')
+ser.write('5')
+print('Wrote: 30')
+time.sleep(5)
+print('Write: .5')
+ser.write('.2/')
+print('Wrote: .5')
 while True:
-     print ser.readline()
+    print ser.read()
+    print('.')
--- Circuit Control/frequency_strobe/frequency_strobe.ino
+++ Circuit Control/frequency_strobe/frequency_strobe.ino
@@ -1,22 +1,48 @@
-int hertz;
+int hertz = -1;
 int period;
-double duty_cycle;
+double duty_cycle = -1;
 int time_high;
 
 
 void setup() {
   pinMode(13, OUTPUT);
-  hertz = 10;
-  duty_cycle = .5;
+  Serial.begin(9600);
+  //hertz = 1;
+  //duty_cycle = .5;
+  bool start_loop = false;
+  String inputString = "";
+  while (hertz < 1) {
+    if (Serial.available() > 0) {
+      hertz = Serial.parseFloat();
+    }
+  }
+  while (duty_cycle <= 0 || duty_cycle > 1) {
+    if (Serial.available() > 0) {
+      duty_cycle = Serial.parseFloat();
+    }
+  }
+  Serial.println(hertz);
+  Serial.println(duty_cycle);
   period = 1000/hertz;
+  Serial.println(period);
   time_high = period*duty_cycle;
+  Serial.println(time_high);
 }
 
 void loop() {
   digitalWrite(13, HIGH);  
-  Serial.print("On");
+  Serial.println("On");
   delay(time_high);
   digitalWrite(13, LOW);  
-  Serial.print("Off");
+  Serial.println("Off");
   delay(period-time_high);
 }
+
+/*void serialEvent() {
+  while (Serial.available()) {
+    digitalWrite(13, HIGH);
+    delay(1000);
+    digitalWrite(13,LOW);
+    delay(1000);
+  }
+}*/
