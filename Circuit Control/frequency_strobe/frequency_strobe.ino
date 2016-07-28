3c3cfa6c8011e80d0d571fb8fc40a2c2739fa62b
GhoulPoP <dylan@lamarca.org>
2016-07-27 18:05:44 -0700

    Strobes at a frequency
    Made arduino code which strobes based on frequency and dutycycle

--- Circuit Control/frequency_strobe/frequency_strobe.ino
+++ Circuit Control/frequency_strobe/frequency_strobe.ino
@@ -1,18 +1,25 @@
-int TOTAL_MICROSECOND = 1000000;
+int TOTAL_MILLISECONDS = 1000;
 int hertz;
 double thigh;
-double tcycle;
+int tcycle;
+double duty_cycle;
 
 
 void setup() {
   pinMode(13, OUTPUT);
-  Hertz = 20;
-  
+  hertz = 1;
+  duty_cycle = .5;
+  calc_duty_cycle();
+}
+
+void calc_duty_cycle() {
+  tcycle = TOTAL_MILLISECONDS/hertz;
+  thigh = tcycle*duty_cycle;
 }
 
 void loop() {
   digitalWrite(13, HIGH);  
-  delay(1000);              // wait for a second
+  delay(thigh);
   digitalWrite(13, LOW);  
-  delay(1000);              // wait for a second
+  delay(tcycle-thigh);
 }
