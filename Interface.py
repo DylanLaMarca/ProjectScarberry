72a999c43fcb6ffa5d770f5a7663264c677ec47a
GhoulPoP <dylan@lamarca.org>
2016-08-16 11:23:18 -0700

    Cleaned up Interface.__init__()
    Shortened Interface.__init__() and made it both prettier and more
    readable

--- Interface.py
+++ Interface.py
@@ -28,9 +28,14 @@ class ScarberryGui:
     save_pic_box = None
     contour_pic = False
     contour_pic_box = None
+    arduino_controller_frame = None
     arduino_controller_text = None
+    ximea_client_frame = None
     ximea_client_text = None
+    process_image_frame = None
     process_image_text = None
+    west_list = []
+    east_list = []
 
     def __init__(self):
         self.master = Tk()
@@ -39,7 +44,31 @@ class ScarberryGui:
         self.master.wm_iconbitmap('icon\\butterflybrown-256.ico')
         self.content = ttk.Frame(self.master)
         frame = ttk.Frame(self.content, borderwidth=2, relief='sunken')
+        extender = Label(self.content, text='                                      ')
+        self.format_menu()
+        self.format_entrys()
+        self.format_optionmenus()
+        self.format_checkboxes()
+        self.format_buttons()
+        self.format_texts()
+        self.content.grid(column=0, row=0)
+        columns = 6
+        rows = 15
+        frame.grid(column=0, row=0, columnspan=columns, rowspan=rows)
+        extender.grid(column=0, row=0,sticky=W)
+        west_count = 0
+        for element in self.west_list:
+            element.grid(column=0, row=west_count, sticky=W, padx=5)
+            west_count+=1
+        east_count = 0
+        for element in self.east_list:
+            element.grid(column=0, row=east_count, sticky=E, padx=5)
+            east_count += 1
+        self.arduino_controller_frame.grid(column=1,row=0,columnspan=2,rowspan=rows,sticky=N,)
+        self.ximea_client_frame.grid(column=1+(columns/3),row=0,columnspan=2,rowspan=rows,sticky=N)
+        self.process_image_frame.grid(column=1+((columns/3)*2),row=0,columnspan=2,rowspan=rows,sticky=N)
 
+    def format_menu(self):
         menu_bar = Menu(self.master, tearoff=0)
         self.master.config(menu=menu_bar)
         file_menu = Menu(menu_bar)
@@ -47,113 +76,96 @@ class ScarberryGui:
         menu_bar.add_cascade(label='File', menu=file_menu)
         settings_menu = Menu(menu_bar)
         preset_menu = Menu(settings_menu)
-        preset_menu.add_command(label='Open Presets',command=self.open_settings)
+        preset_menu.add_command(label='Open Presets', command=self.open_settings)
         preset_menu.add_command(label='Set Presets', command=self.set_presets)
-        preset_menu.add_command(label='Reset to Presets',command=self.set_entry)
+        preset_menu.add_command(label='Reset to Presets', command=self.set_entry)
         settings_menu.add_cascade(label='Presets', menu=preset_menu, underline=0)
         menu_bar.add_cascade(label='Settings', menu=settings_menu)
         menu_bar.add_command(label='About')
 
-        west_list = []
-        east_list = []
-        extender = Label(self.content,text='                                      ')
+    def format_entrys(self):
         self.com = StringVar()
-        com_label = Label(self.content,text='Serial Port:   COM')
-        west_list.append(com_label)
-        self.com_entry = Entry(self.content,width=3,textvariable=self.com)
-        east_list.append(self.com_entry)
+        com_label = Label(self.content, text='Serial Port:   COM')
+        self.west_list.append(com_label)
+        self.com_entry = Entry(self.content, width=3, textvariable=self.com)
+        self.east_list.append(self.com_entry)
         self.runtime = StringVar()
-        runtime_label = Label(self.content,text='RunTime:  ')
-        west_list.append(runtime_label)
-        self.runtime_entry = Entry(self.content,width=5,textvariable=self.runtime)
-        east_list.append(self.runtime_entry)
+        runtime_label = Label(self.content, text='RunTime:  ')
+        self.west_list.append(runtime_label)
+        self.runtime_entry = Entry(self.content, width=5, textvariable=self.runtime)
+        self.east_list.append(self.runtime_entry)
         self.framerate = StringVar()
-        framerate_label = Label(self.content,text='Framerate:  ')
-        west_list.append(framerate_label)
-        self.framerate_entry = Entry(self.content,width=5,textvariable=self.framerate)
-        east_list.append(self.framerate_entry)
+        framerate_label = Label(self.content, text='Framerate:  ')
+        self.west_list.append(framerate_label)
+        self.framerate_entry = Entry(self.content, width=5, textvariable=self.framerate)
+        self.east_list.append(self.framerate_entry)
         self.strobecount = StringVar()
-        strobecount_label = Label(self.content,text='Strobe Count:  ')
-        west_list.append(strobecount_label)
-        self.strobecount_entry = Entry(self.content,width=5,textvariable=self.strobecount)
-        east_list.append(self.strobecount_entry)
+        strobecount_label = Label(self.content, text='Strobe Count:  ')
+        self.west_list.append(strobecount_label)
+        self.strobecount_entry = Entry(self.content, width=5, textvariable=self.strobecount)
+        self.east_list.append(self.strobecount_entry)
         self.dutycycle = StringVar()
-        dutycycle_label = Label(self.content,text='Duty Cycle:        0.')
-        west_list.append(dutycycle_label)
-        self.dutycycle_entry = Entry(self.content,width=2,textvariable=self.dutycycle)
-        east_list.append(self.dutycycle_entry)
+        dutycycle_label = Label(self.content, text='Duty Cycle:        0.')
+        self.west_list.append(dutycycle_label)
+        self.dutycycle_entry = Entry(self.content, width=2, textvariable=self.dutycycle)
+        self.east_list.append(self.dutycycle_entry)
         self.thresh = StringVar()
-        thresh_label = Label(self.content,text='Thresh Limit:  ')
-        west_list.append(thresh_label)
-        self.thresh_entry = Entry(self.content,width=5,textvariable=self.thresh)
-        east_list.append(self.thresh_entry)
+        thresh_label = Label(self.content, text='Thresh Limit:  ')
+        self.west_list.append(thresh_label)
+        self.thresh_entry = Entry(self.content, width=5, textvariable=self.thresh)
+        self.east_list.append(self.thresh_entry)
 
+    def format_optionmenus(self):
         self.blur_options = self.generate_optionmenu_elements(15, 3, 2)
-        self.blur = IntVar(self.master,value=self.blur_options[0])
-        blur_label =  Label(self.content,text='Blur Val:  ')
-        west_list.append(blur_label)
+        self.blur = IntVar(self.master, value=self.blur_options[0])
+        blur_label = Label(self.content, text='Blur Val:  ')
+        self.west_list.append(blur_label)
         self.blur_menu = apply(OptionMenu, (self.content, self.blur) + tuple(self.blur_options))
-        east_list.append(self.blur_menu)
+        self.east_list.append(self.blur_menu)
         self.blur_menu.pack()
         self.gain_options = self.generate_optionmenu_elements(7, 0, 1)
-        self.gain = IntVar(self.master,value=self.gain_options[6])
-        gain_label =  Label(self.content,text='Gain Val:  ')
-        west_list.append(gain_label)
+        self.gain = IntVar(self.master, value=self.gain_options[6])
+        gain_label = Label(self.content, text='Gain Val:  ')
+        self.west_list.append(gain_label)
         self.gain_menu = apply(OptionMenu, (self.content, self.gain) + tuple(self.gain_options))
-        east_list.append(self.gain_menu)
+        self.east_list.append(self.gain_menu)
         self.gain_menu.pack()
 
+    def format_checkboxes(self):
         self.save_pic = IntVar()
         self.save_pic_box = Checkbutton(self.content, text='Save Pic', variable=self.save_pic)
-        west_list.append(self.save_pic_box)
-        east_list.append(Label(self.content))
+        self.west_list.append(self.save_pic_box)
+        self.east_list.append(Label(self.content))
         self.save_pic_box.pack()
         self.contour_pic = IntVar()
         self.contour_pic_box = Checkbutton(self.content, text='Contour Pic', variable=self.contour_pic)
-        west_list.append(self.contour_pic_box)
-        east_list.append(Label(self.content))
+        self.west_list.append(self.contour_pic_box)
+        self.east_list.append(Label(self.content))
         self.contour_pic_box.pack()
 
-        start_button = Button(self.content, text='   Start   ',command=self.start_main)
+    def format_buttons(self):
+        start_button = Button(self.content, text='   Start   ', command=self.start_main)
         start_button.pack()
-        west_list.append(start_button)
+        self.west_list.append(start_button)
         abort_button = Button(self.content, text='   Abort   ')
-        start_button.pack()
-        east_list.append(abort_button)
+        abort_button.pack()
+        self.east_list.append(abort_button)
 
+    def format_texts(self):
         text_hight = 15
         text_width = 30
-        arduino_controller_frame = LabelFrame(self.content, text='ArduinoController', padx=5, pady=5)
-        self.arduino_controller_text = Text(arduino_controller_frame, height=text_hight, width=text_width)
+        self.arduino_controller_frame = LabelFrame(self.content, text='ArduinoController', padx=5, pady=5)
+        self.arduino_controller_text = Text(self.arduino_controller_frame, height=text_hight, width=text_width)
         self.arduino_controller_text.config(state=DISABLED)
         self.arduino_controller_text.pack()
-        ximea_client_frame = LabelFrame(self.content, text='XimeaClient', padx=5, pady=5)
-        self.ximea_client_text = Text(ximea_client_frame, height=text_hight, width=text_width)
+        self.ximea_client_frame = LabelFrame(self.content, text='XimeaClient', padx=5, pady=5)
+        self.ximea_client_text = Text(self.ximea_client_frame, height=text_hight, width=text_width)
         self.ximea_client_text.config(state=DISABLED)
         self.ximea_client_text.pack()
-        process_image_frame = LabelFrame(self.content, text='ProcessImage', padx=5, pady=5)
-        self.process_image_text = Text(process_image_frame, height=text_hight, width=text_width)
+        self.process_image_frame = LabelFrame(self.content, text='ProcessImage', padx=5, pady=5)
+        self.process_image_text = Text(self.process_image_frame, height=text_hight, width=text_width)
         self.process_image_text.config(state=DISABLED)
         self.process_image_text.pack()
-
-        self.content.grid(column=0, row=0)
-        columns = 6
-        rows = 15
-        frame.grid(column=0, row=0, columnspan=columns, rowspan=rows)
-
-        extender.grid(column=0, row=0,sticky=W)
-        west_count = 0
-        for element in west_list:
-            element.grid(column=0, row=west_count, sticky=W, padx=5)
-            west_count+=1
-        east_count = 0
-        for element in east_list:
-            element.grid(column=0, row=east_count, sticky=E, padx=5)
-            east_count += 1
-
-        arduino_controller_frame.grid(column=1,row=0,columnspan=2,rowspan=rows,sticky=N,)
-        ximea_client_frame.grid(column=1+(columns/3),row=0,columnspan=2,rowspan=rows,sticky=N)
-        process_image_frame.grid(column=1+((columns/3)*2),row=0,columnspan=2,rowspan=rows,sticky=N)
 
     def start(self):
         self.master.mainloop()
