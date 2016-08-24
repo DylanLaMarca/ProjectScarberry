c73bcf4a5657cc7db574b5cae7e5352d2de1cce2
GhoulPoP <dylan@lamarca.org>
2016-08-24 14:46:10 -0700

    ProcessImage gets Data
    ProcessImage now has an improved way of drawing on images, all from one
    method using keyword arguments, and can take images and extrapolate all
    of their contour cluster's centroid, axes, direction, perimeter, and
    vector and save the information to a file

--- ProcessImage.py
+++ ProcessImage.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import Interface
 
 class ProcessImage:
     @staticmethod
@@ -41,44 +40,24 @@ class ProcessImage:
         return vectors
 
     @staticmethod
-    def draw_vectors(in_image,out_image,blur_val,thresh_val):
-        contours = ProcessImage.get_contours(in_image, blur_val, thresh_val)
-        for cnt in contours:
-            rows, cols = in_image.shape[:2]
-            [vx, vy, x, y] = cv2.fitLine(cnt, cv2.DIST_L2, 0, 0.01, 0.01)
-            lefty = int((-x * vy / vx) + y)
-            righty = int(((cols - x) * vy / vx) + y)
-            cv2.line(in_image, (cols - 1, righty), (0, lefty), (0, 0, 255), 1)
-        cv2.imwrite(out_image,in_image)
-        return in_image
-
-    @staticmethod
-    def draw_rois(in_image,out_image,blur_val,thresh_val):
-        contours = ProcessImage.get_contours(in_image, blur_val, thresh_val)
-        rois = range(len(contours))
-        count = 0
-        for cnt in contours:
-            x, y, w, h = cv2.boundingRect(cnt)
-            cv2.rectangle(in_image, (x, y), (x + w, y + h), (0, 0, 255), 1)
-            count += 1
-        cv2.imwrite(out_image, in_image)
-        return in_image
-
-    @staticmethod
-    def maximize_rois(image, incrament, blur_val):
-        incrament = 50.0
-        value = incrament*-1
-        roi_num = 0
-        count = 0
-        while (incrament > 1):
-            current_roi_num = ProcessImage.get_contours(image,blur_val,value+incrament)
-            ProcessImage.draw_rois(image,'images\\image119rois{}.png'.format(value+incrament),)
-            if(current_roi_num >= roi_num):
-                roi_num = current_roi_num
-            elif(current_roi_num < roi_num):
-                incrament *= .5
-            value+=incrament
-            count+=1
+    def draw(image,out_image,blur_val,thresh_val,rois=False,centroid=False,vectors=False,colour=(0,0,255)):
+        contours = ProcessImage.get_contours(image, blur_val, thresh_val)
+        for contour in contours:
+            if rois:
+                x, y, w, h = cv2.boundingRect(contour)
+                cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 1)
+            if centroid:
+                moments = cv2.moments(contour)
+                centroid_x = int(moments['m10'] / moments['m00'])
+                centroid_y = int(moments['m01'] / moments['m00'])
+                cv2.circle(image, (centroid_x, centroid_y), 1, (0, 0, 255), -1)
+            if vectors:
+                rows, cols = image.shape[:2]
+                [vx, vy, x, y] = cv2.fitLine(contour, cv2.DIST_L2, 0, 0.01, 0.01)
+                lefty = int((-x * vy / vx) + y)
+                righty = int(((cols - x) * vy / vx) + y)
+                cv2.line(image, (cols - 1, righty), (0, lefty), (0, 0, 255), 1)
+        ProcessImage.show_image(image)
 
     @staticmethod
     def show_image(img):
@@ -87,14 +66,45 @@ class ProcessImage:
         cv2.destroyAllWindows()
 
     @staticmethod
-    def save_images(images,name,file_type):
-        count = 0
-        for image in images:
-            cv2.imwrite("{}{}.{}".format(name,count,file_type),image)
-            count+=1
+    def get_data(image,blur_val,thresh_val,):
+        contours = ProcessImage.get_contours(image, blur_val, thresh_val)
+        data = []
+        for contour in contours:
+                current_roi = {}
+                moments = cv2.moments(contour)
+                centroid_x = int(moments['m10'] / moments['m00'])
+                centroid_y = int(moments['m01'] / moments['m00'])
+                current_roi['Centroid'] = (centroid_x,centroid_y)
+                [vx, vy, x, y] = cv2.fitLine(contour, cv2.DIST_L2, 0, 0.01, 0.01)
+                current_roi['Vector'] = {'vx':vx[0],'vy':vy[0],'x':x[0],'y':y[0]}
+                area = cv2.contourArea(contour)
+                current_roi['Area'] = area
+                perimeter = cv2.arcLength(contour, True)
+                current_roi['ApproximatePerimeter'] = perimeter
+                (x, y), (MA, ma), angle = cv2.fitEllipse(contour)
+                current_roi['Axes'] = {'Major Axis':MA,'Minor Axis':ma}
+                current_roi['Direction'] = angle
+                data.append(current_roi)
+        return data
 
-    def chose_print(self, gui, text):
-        if (gui == None):
-            print
-        else:
-            gui.print_to_process_text(text)
+    @staticmethod
+    def save_data(data,file_name):
+        filename = file_name
+        file = open(filename, 'w')
+        file.truncate()
+        count = 0
+        for contour in data:
+            file.write('Contour Cluster {}:\n'.format(count))
+            file.write('    Centroid: {}\n'.format(contour.get('Centroid')))
+            file.write('    Axes:\n')
+            file.write('        Major Axis: {}\n'.format(contour.get('Axes').get('Major Axis')))
+            file.write('        Minor Axis: {}\n'.format(contour.get('Axes').get('Minor Axis')))
+            file.write('    Direction: {}\n'.format(contour.get('Direction')))
+            file.write('    ApproximatePerimeter: {}\n'.format(contour.get('ApproximatePerimeter')))
+            file.write('    Vector:\n')
+            file.write('        x:  {}\n'.format(contour.get('Vector').get('x')))
+            file.write('        y:  {}\n'.format(contour.get('Vector').get('y')))
+            file.write('        vx: {}\n'.format(contour.get('Vector').get('vx')))
+            file.write('        vy: {}\n'.format(contour.get('Vector').get('vy')))
+            count+=1
+        file.close()
