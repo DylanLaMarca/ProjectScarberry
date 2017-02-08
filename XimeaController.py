09228bf0801057012ffe02be3a8889ef1c7b3a63
Dylan Michael LaMarca <dylan@lamarca.org>
2017-02-07 17:46:54 -0800

    Discarded outdated xiAPI work
    Deleted the C# XimeaController and XimeaClient which interacted with
    XimeaController, replacing it with XimeaController.py

---
+++ XimeaController.py
@@ -0,0 +1,47 @@
+"""
+Contains all of the code needed to interact and utilize the C# program XimeaController for ProjectScarberry.
+    :author: Dylan Michael LaMarca
+    :contact: dylan@lamarca.org
+    :GitHub: https://github.com/GhoulPoP/ProjectScarberry
+    :Date: 26/7/2016 - 7/2/2017
+"""
+from ximea import xiapi
+import cv2
+
+#create instance for first connected camera
+cam = xiapi.Camera()
+#start communication
+print('Opening first camera...')
+cam.open_device()
+
+#settings
+cam.set_exposure(20000)
+
+#create instance of Image to store image data and metadata
+img = xiapi.Image()
+
+#start data acquisition
+print('Starting data acquisition...')
+cam.start_acquisition()
+
+#get data and pass them from camera to img
+cam.get_image(img)
+
+#create numpy array with data from camera. Dimensions of array are determined
+#by imgdataformat
+data = img.get_image_data_numpy()
+
+#stop data acquisition
+print('Stopping acquisition...')
+cam.stop_acquisition()
+
+#stop communication
+cam.close_device()
+
+#show acquired image
+print('Drawing image...')
+cv2.imshow('XiCAM example', data)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+print('Done.')
