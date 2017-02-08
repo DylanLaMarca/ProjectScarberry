9d1fe4fa50488a4f2a474295371ad2c67041863b
Dylan Michael LaMarca <dylan@lamarca.org>
2017-02-07 17:47:41 -0800

    Updated Documentation Contact Information
    Changed all refferences to dlamarca@uw.edu to dylan@lamarca.org

--- Circuit Control/frequency_trigger/frequency_trigger.ino
+++ Circuit Control/frequency_trigger/frequency_trigger.ino
@@ -8,9 +8,9 @@
     ~ pin 11: IR Strobe Curcuit Optic Isolator
  ************************************************
   Created:  4/8/2016
-    Dylan Michael LaMarca
+    Dylan Michael LaMarca [dylan@lamarca.org]
   Modified: 2/9/2016
-    Dylan Michael LaMarca
+    Dylan Michael LaMarca [dylan@lamarca.org]
   Github:
     https://github.com/GhoulPoP/ProjectScarberry
 */
